6d07699ebd175ae58a60549e40c708539501d3a4
playboikz <acorlings@gmail.com>
2020-04-01 14:43:16 -0400

    La taille de la queue est <14>

--- b000000000.py
+++ b000000000.py
@@ -3,9 +3,25 @@
 
 @author: playboikz
 """
+eleves = {}
+eleves["Boris"]=["Amir","Franck","Nathalie","Bertrand"]
+eleves["Amir"]=[]
+eleves["Franck"]=[]
+eleves["Nathalie"]=[]
+eleves["Bertrand"]=["Erna","Hassana","Abdelkrim"]
+eleves["Erna"]=[]
+eleves["Hassana"]=[]
+eleves["Zoureni"]=["Sekou","Auriane","Corlings"]
+eleves["Sekou"]=[]
+eleves["Auriane"]=[]
+eleves["Corlings"]=[]
+eleves["Abdelkrim"]=["Souleyman","Zack","Zoureni"]
+eleves["Souleyman"]=[]
+eleves["Zack"]=[]
 
-def main():
-   print('Informatique: le rêve')
+def search(name):
+   print( len(eleves.values()) )
+   return False
 
 if __name__== "__main__":
-  main()
+   search("Boris")
