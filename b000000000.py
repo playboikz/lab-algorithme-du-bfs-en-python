291eb9b7122d17971b0cea0646e136e5bf2bad8d
playboikz <acorlings@gmail.com>
2020-04-01 15:05:56 -0400

    Tutoriel complete

--- b000000000.py
+++ b000000000.py
@@ -19,9 +19,42 @@ eleves["Abdelkrim"]=["Souleyman","Zack","Zoureni"]
 eleves["Souleyman"]=[]
 eleves["Zack"]=[]
 
+def personne_elue(name):
+    return name == 'Zoureni'
+
+from collections import deque
+
 def search(name):
-   print( len(eleves.values()) )
+   visitees = []
+   search_queue = deque()
+   search_queue += eleves[name]
+   while search_queue:
+      personne = search_queue.popleft()
+ if not personne in visitees:
+         if personne_elue(personne):
+            ...
+def search(name):
++   visitees = []
+    search_queue = deque()
+    search_queue += eleves[name]
+-   print( len(eleves.values()) )
++   while search_queue:
++      personne = search_queue.popleft()
++      if not personne in visitees:
++         if personne_elue(personne):
++            print(personne + " a le fameux Mac")
++            return True
++         search_queue += eleves[personne]
++         visitees.append(personne)
+    return False
+
+      if personne_elue(personne):
+         print(personne + " a le fameux Mac")
+         return True
+      search_queue += eleves[personne]
    return False
+
+
 
 if __name__== "__main__":
    search("Boris")
